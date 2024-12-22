d0c8fa1aa5b9326021b464159fcdf91d6f2705a9
jj <twojemail@example.com>
2024-12-22 17:36:06 +0100

    Add Player class with properties and UpdateScore method

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Player
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int Score { get; set; }
+
+    public Player(string name, string position, int score)
+    {
+        Name = name;
+        Position = position;
+        Score = score;
+    }
+    
+    public void UpdateScore(int newScore)
+    {
+        Score = newScore;
+    }
+}
